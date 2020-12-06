5c94784be3b58348d6ed04f1b0f2611bd8598f54
Krisz Kaszas <kriszrokakaszas@gmail.com>
2020-12-06 14:06:57 +0100

    done

---
+++ SegmentLength/SegmentLength/SegmentLength.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void createSegment(int length)
+{
+	for (int i = 0; i < length; i++)
+		printf_s("-");
+}
+
+void printSegment(int length)
+{
+	printf_s("+");
+	createSegment(length);
+	printf_s("+");
+}
+
+int main(void)
+{
+	float length;
+	printf_s("Segment Creator \n Please specify segmant length:");
+	scanf_s("%f", &length);
+	printSegment(length);
+	return 0;
+}
